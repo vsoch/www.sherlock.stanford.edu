45a5deedf25668cff0bf3a5505d285bdfe32fc58
Kilian Cavalotti <kilian@stanford.edu>
2017-05-17 12:25:06 -0700

    conditional scrolling, only for internal anchor links

--- js/shweb.js
+++ js/shweb.js
@@ -1,4 +1,4 @@
-// Agency Theme JavaScript
+// Theme JavaScript
 
 (function($) {
     "use strict"; // Start of use strict
@@ -6,10 +6,12 @@
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+        if ($anchor.attr('href').startsWith('#')) {
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        }
     });
 
     // Highlight the top nav as scrolling occurs
